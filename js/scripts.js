f6cd03daafa0e077fd80fa8b30e76c32572bd45c
Cristian Lucero <cristianjlucero32@gmail.com>
2018-10-19 11:24:52 -0700

    Fixed mathematical algorithm of results.

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
   var operator1 = parseInt($("input:radio[name=question1]:checked").val());
   var operator2 = parseInt($("input:radio[name=question2]:checked").val());
   var operator3 = parseInt($("input:radio[name=question3]:checked").val());
+  var operator4 = parseInt($("input:radio[name=question4]:checked").val());
+  var operator5 = parseInt($("input:radio[name=question5]:checked").val());
 
-  var sum = (operator1 + operator2 + operator3);
+  var sum = (operator1 + operator2 + operator3 + operator4 + operator5);
 
-  if (sum <5) {
+  if (sum <=4) {
     $(".c").show();
     $(".css").hide();
     $(".java").hide();
@@ -20,7 +22,7 @@ $(document).ready(function() {
     $(".java").hide();
   }
 
-  else if (sum >7) {
+  else if (sum >=8) {
     $(".c").hide();
     $(".css").hide();
     $(".java").show();
